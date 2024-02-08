38b396ed1d0e0e9d46c8eb4782250178983b7e97
Shawn Crook <shawncrook411@gmail.com>
2024-02-07 20:24:46 -0700

    removed old code

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,25 +1,8 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_TECH = gql`
-  query tech {
-    tech {
-      _id
-      name
-    }
-  }
-`;
+export const QUERY_TECH = gql`x`;
 
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
-    }
-  }
-`;
+export const QUERY_MATCHUPS = gql`x`;
 
 export const USER = gql`
   query user($username: username, $email: email) {
@@ -44,7 +27,7 @@ export const USER = gql`
       }
     }
   }`
-  
+
 export const USERS = gql`
   query users {
     users {
