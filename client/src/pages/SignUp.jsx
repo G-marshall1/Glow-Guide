c3d57b7594768e82e82da827456edd524377651e
Shawn Crook <shawncrook411@gmail.com>
2024-02-08 21:00:09 -0700

    captial

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 import { useMutation } from '@apollo/client'
 import { ADD_USER } from '../utils/mutations';
-import Auth from '..utils/auth'
-
-
+import Auth from '../utils/auth'
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
